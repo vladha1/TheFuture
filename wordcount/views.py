b87c4983372375f08fc3e05ac3fb0a3810fdb19d
vladha1 <vladha@gmail.com>
2021-04-13 10:07:17 +0530

    commit message

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,43 +7,32 @@ import uuid
 from datetime import datetime
 from decimal import Decimal
 import requests
-from bs4 import BeautifulSoup
-import investpy
-import feedparser
 
+from newscatcher import Newscatcher
+from newscatcher import urls
 
-def timesofindia():
-    url = "https://timesofindia.indiatimes.com/home/headlines"
-    page_request = requests.get(url)
-    data = page_request.content
-    soup = BeautifulSoup(data,"html.parser")
-
-    counter = 0
-    response=[]
-    for divtag in soup.find_all('div', {'class': 'headlines-list'}):
-        for ultag in divtag.find_all('ul', {'class': 'clearfix'}):
-            if (counter <= 10):
-                for litag in ultag.find_all('li'):
-                    counter = counter + 1
-                    #print(str(counter) + " - https://timesofindia.indiatimes.com" + litag.find('a')['href'])
-                    response=response+[{'counter':str(counter),'headline':litag.text,'link': "https://timesofindia.indiatimes.com" + litag.find('a')['href']}]
-    return response
-
-def invest():
-    df = investpy.get_stock_historical_data(stock='AAPL',country='United States',from_date='01/04/2021',to_date='10/04/2021')
-    return df.to_html
-
-
-def rbi():
-    feedresults=[]
-    feed = feedparser.parse('https://rbi.org.in/pressreleases_rss.xml')
-    for items in feed.entries:
-        feedresults=feedresults+[{'title':items.title,'published':items.published,'link':items.link}]
-    feeddata=pd.DataFrame(feedresults).to_html
-    return feeddata
 
 
 def home(request):
-        return render(request,'home.html',{'news':timesofindia(),'tickers':invest(),'rbi':rbi()})
+    newslist=[]
 
+    IndianURLs = urls(country = 'IN')
+    
+    det=[]
+
+    for IndianURL in IndianURLs:
+
+        nc = Newscatcher(website = IndianURL)
+        results = nc.get_news()
+        print(nc)
+        try:
+            articles = results['articles']
+        
+            for article in articles:
+                newslist=newslist+[{'Source':IndianURL,'Title':article.title,'Published':article.published,'Summary_Detail':list(article.summary_detail.values())[3],'link':article.link}]
+        except:
+            a=1
+        print(newslist)
+    return render(request, 'home.html', {'newslist':newslist})
+        #return newslist
 
---
+++ wordcount/viewsy.py
@@ -0,0 +1,71 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pandas as pd
+import operator
+import time
+import uuid
+from datetime import datetime
+from decimal import Decimal
+import requests
+from bs4 import BeautifulSoup
+import investpy
+#import feedparser
+
+
+def timesofindia():
+    url = "https://timesofindia.indiatimes.com/home/headlines"
+    page_request = requests.get(url)
+    data = page_request.content
+    soup = BeautifulSoup(data,"html.parser")
+
+    counter = 0
+    response=[]
+    for divtag in soup.find_all('div', {'class': 'headlines-list'}):
+        for ultag in divtag.find_all('ul', {'class': 'clearfix'}):
+            if (counter <= 10):
+                for litag in ultag.find_all('li'):
+                    counter = counter + 1
+                    #print(str(counter) + " - https://timesofindia.indiatimes.com" + litag.find('a')['href'])
+                    response=response+[{'counter':str(counter),'headline':litag.text,'link': "https://timesofindia.indiatimes.com" + litag.find('a')['href']}]
+    return response
+
+def invest():
+    df = investpy.get_stock_historical_data(stock='AAPL',country='United States',from_date='01/04/2021',to_date='10/04/2021')
+    return df.to_html
+
+
+def rbi():
+    feedresults=[]
+    feed = feedparser.parse('https://rbi.org.in/pressreleases_rss.xml')
+    for items in feed.entries:
+        feedresults=feedresults+[{'title':items.title,'published':items.published,'link':items.link}]
+    feeddata=pd.DataFrame(feedresults).to_html
+    return feeddata
+
+
+def home1(request):
+        return render(request,'home.html',{'news':timesofindia(),'tickers':invest(),'rbi':rbi()})
+
+
+def home(request):
+    newslist=[]
+
+    IndianURLs = urls(country = 'IN')
+
+    det=[]
+
+    for IndianURL in IndianURLs:
+
+        nc = Newscatcher(website = IndianURL)
+        results = nc.get_news()
+        
+        try:
+            articles = results['articles']
+        
+            for article in articles:
+                newslist=newslist+[{'Source':IndianURL,'Title':article.title,'Published':article.published,'Summary_Detail':list(article.summary_detail.values())[3],'link':article.link}]
+        except:
+            a=1
+        render(request, 'myapp/index.html', {'newslist':newslist})
+        #return newslist
+
