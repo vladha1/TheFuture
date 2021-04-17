7c2af0b4e81b6ade5d248bd9322c53afe7494200
vladha1 <vladha@gmail.com>
2021-04-17 17:12:24 +0530

    great

--- wordcount/views.py
+++ wordcount/views.py
@@ -13,8 +13,7 @@ from newscatcher import urls
 from datetime import datetime
 import datefinder
 from operator import itemgetter
-
-
+import feedparser
 
 def home(request):
     newslist=[]
@@ -44,13 +43,48 @@ def home(request):
                 txt=list(article.summary_detail.values())[3]
                 detailtext = BeautifulSoup(txt, "html.parser").get_text()                
                 
-                if searchcriteria==None or searchcriteria in detailtext or searchcriteria in article.title:               
-                    counter=counter+1
-                    newslist=newslist+[{'Source':IndianURL,'Title':article.title,'Published':dateresult,'Summary_Detail':detailtext,'link':article.link,'id':"head_"+str(counter)}]
-                
-                newslist_sorted=sorted(newslist, key= lambda i: i['Published'],reverse=True)
+            if searchcriteria==None or searchcriteria in detailtext or searchcriteria in article.title:               
+                counter=counter+1
+                newslist=newslist+[{'Source':IndianURL,'Title':article.title,'Published':dateresult,'Summary_Detail':detailtext,'link':article.link,'id':"head_"+str(counter)}]
+
+    newslist=newslist+rssfeeds(searchcriteria)
+    newslist_sorted=sorted(newslist, key= lambda i: i['Published'],reverse=True)
                 #newslist_sorted=newslist_sorted[newslist_sorted['Summary_Detail'].str.contains("Hwang")]
-            
+    
     print("responding")
     return render(request, 'home.html', {'newslist':newslist_sorted})
         #return newslist
+
+def rssfeeds(searchcriteria):
+
+    feedsources=['https://www.livemint.com/rss/news','https://www.financialexpress.com/feed/','https://www.news18.com/rss/business.xml','https://www.business-standard.com/rss/latest.rss','https://economictimes.indiatimes.com/rssfeedsdefault.cms','https://www.moneycontrol.com/rss/MCtopnews.xml','https://www.thehindu.com/business/feeder/default.rss']
+    news=[]
+    counter=0
+    for feedsource in feedsources:
+        
+        NewsFeed = feedparser.parse(feedsource)
+        for items in NewsFeed.entries:
+            newsitem={}
+            if searchcriteria==None or searchcriteria in items.summary or searchcriteria in items.title:               
+                counter=counter+1
+                newsitem['id']="head1_"+str(counter)
+                newsitem['Source']=NewsFeed.feed.title
+                if NewsFeed.feed.title=="Latest News":
+                    newsitem['Source']="Business Standard"
+                newsitem['Title']=items.title
+                newsitem['Summary_Detail']=items.summary
+                newsitem['link']=items.link
+                datesfound=datefinder.find_dates(items.published)
+                dateresult="x"
+                for match in datesfound:
+                    dateresult=match.strftime("%Y-%m-%d %H:%M")
+                
+                #dateresult="2021-04-18"
+                newsitem['Published']=dateresult
+                news=news+[newsitem]
+
+            
+    #print(NewsFeed.feed.title)
+            
+    return news
+
