2b8ed90eb6999b55ec378d8b5e3686b666393aac
vladha1 <vladha@gmail.com>
2021-07-11 17:09:32 +0530

    commit message

--- wordcount/views.py
+++ wordcount/views.py
@@ -120,8 +120,8 @@ def rssfeeds():
 def twitter():
     tweetnews=[]
 
-    auth = tweepy.OAuthHandler("59X78YGbAo6BQ5QefdmQmtYmj", "JCebH7bgEtOFSUAi5Y6dIum45YsGjBd8oSwU7glcUaPoKaTcx4")
-    auth.set_access_token("572792793-RK5PLEtoDdkLog2D5um7xlHXhKmwMJG3UsLFk7jH","Umibys5vQwD4DV5joRrQrvyZezXvt34LDXnjywuW1bMAV" )
+    auth = tweepy.OAuthHandler("")
+    auth.set_access_token("" )
 
 
 
